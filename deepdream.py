19fb14d0b63929d372b6afb9e24a6d7ab73169c1
Sherlock <sherlockliao01@gmail.com>
2017-07-23 15:42:50 +0800

    update
    finish ipynb update

--- deepdream.py
+++ deepdream.py
@@ -1,15 +1,15 @@
 import numpy as np
-import torch as t
+import torch
 from util import showtensor
 import scipy.ndimage as nd
-from torch.autograd import Variable as v
+from torch.autograd import Variable
 
 
 def objective_L2(dst, guide_features):
     return dst.data
 
 
-def make_step(img, model, control=None):
+def make_step(img, model, control=None, distance=objective_L2):
     mean = np.array([0.485, 0.456, 0.406]).reshape([3, 1, 1])
     std = np.array([0.229, 0.224, 0.225]).reshape([3, 1, 1])
 
@@ -18,19 +18,18 @@ def make_step(img, model, control=None):
     num_iterations = 20
     show_every = 10
     end_layer = 3
-    distance = objective_L2
     guide_features = control
 
     for i in range(num_iterations):
-        shift_x, shift_y = np.random.randint(-max_jitter, max_jitter+1, 2)
+        shift_x, shift_y = np.random.randint(-max_jitter, max_jitter + 1, 2)
         img = np.roll(np.roll(img, shift_x, -1), shift_y, -2)
         # apply jitter shift
         model.zero_grad()
-        img_tensor = t.Tensor(img)
-        if t.cuda.is_available():
-            img_variable = v(img_tensor.cuda(), requires_grad=True)
+        img_tensor = torch.Tensor(img)
+        if torch.cuda.is_available():
+            img_variable = Variable(img_tensor.cuda(), requires_grad=True)
         else:
-            img_variable = v(img_tensor, requires_grad=True)
+            img_variable = Variable(img_tensor, requires_grad=True)
 
         act_value = model.forward(img_variable, end_layer)
         diff_out = distance(act_value, guide_features)
@@ -47,21 +46,28 @@ def make_step(img, model, control=None):
     return img
 
 
-def dream(model, base_img, octave_n=6, octave_scale=1.4):
+def dream(model,
+          base_img,
+          octave_n=6,
+          octave_scale=1.4,
+          control=None,
+          distance=objective_L2):
     octaves = [base_img]
-    for i in range(octave_n-1):
-        octaves.append(nd.zoom(octaves[-1],
-                               (1, 1, 1.0 / octave_scale, 1.0 / octave_scale),
-                               order=1))
+    for i in range(octave_n - 1):
+        octaves.append(
+            nd.zoom(
+                octaves[-1], (1, 1, 1.0 / octave_scale, 1.0 / octave_scale),
+                order=1))
 
     detail = np.zeros_like(octaves[-1])
     for octave, octave_base in enumerate(octaves[::-1]):
         h, w = octave_base.shape[-2:]
         if octave > 0:
             h1, w1 = detail.shape[-2:]
-            detail = nd.zoom(detail, (1, 1, 1.0*h / h1, 1.0*w / w1), order=1)
+            detail = nd.zoom(
+                detail, (1, 1, 1.0 * h / h1, 1.0 * w / w1), order=1)
 
         input_oct = octave_base + detail
         print(input_oct.shape)
-        out = make_step(input_oct, model)
+        out = make_step(input_oct, model, control, distance=distance)
         detail = out - octave_base
